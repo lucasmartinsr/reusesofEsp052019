ee15116c0744033630d91e093ff5065bc6bc45f1
Lucas Martins <lucas_mr92@hotmail.com>
2019-05-18 10:09:50 -0300

    Criando projeto

---
+++ factoryMethod/factoryMethod/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace factoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
